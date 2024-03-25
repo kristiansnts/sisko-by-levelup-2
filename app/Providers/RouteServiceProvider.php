6817eb3a30433242e256984737d8669f16d44c6a
kristiansnts <epafroditus.kristian@gmail.com>
2024-03-25 14:44:45 +0700

    init

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider
+{
+
+    public const HOME = '/';
+    public const DASHBOARD = '/dashboard';
+
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
